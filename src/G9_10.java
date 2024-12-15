649a88d19b2e3339cae45847ff3aa591e82c7e7c
kobocawaee <112955078+kobocawaee@users.noreply.github.com>
2024-12-15 18:20:19 +0800

    Add files via upload

---
+++ src/G9_10.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class G9_10 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+      
+        Scene scene = new Scene(root);
+        stage.setTitle("圓射!啟動!!");
+        stage.setScene(scene);
+        stage.show();
+   }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
