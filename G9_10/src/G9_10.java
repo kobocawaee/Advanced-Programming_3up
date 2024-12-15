3b55076918ebf2185b0c028511d1291a2b4ab09c
Jeffrey <a31185367@gmail.com>
2024-12-15 19:52:40 +0800

    1. 新增背景圖片、icon。 2. 將image2.png調整為去背。 3. 將Score、Time、Combo調整大小為25pt、白色並將其lable尺寸調整讓其能完整顯示。 4. 將按鈕設置圖片讓其融合進背景圖片。
    Signed-off-by: <Jeffrey> <<a31185367@gmail.com>>

--- G9_10/src/G9_10.java
+++ G9_10/src/G9_10.java
@@ -3,24 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
  */
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+ import javafx.application.Application;
+ import javafx.fxml.FXMLLoader;
+ import javafx.scene.Parent;
+ import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-public class G9_10 extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
-      
-        Scene scene = new Scene(root);
-        stage.setTitle("圓射!啟動!!");
-        stage.setScene(scene);
-        stage.show();
-   }
-
-    public static void main(String[] args) {
-        launch(args);
+ 
+ public class G9_10 extends Application {
+     @Override
+     public void start(Stage stage) throws Exception {
+         Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+         Scene scene = new Scene(root);
+         Image img =  new Image("icon.jpg"); //縮圖及背景圖片
+         stage.getIcons().add(img);
+         stage.setTitle("圓射!啟動!!");
+         stage.setScene(scene);
+         stage.show();
     }
-}
+ 
+     public static void main(String[] args) {
+         launch(args);
+     }
+ }
