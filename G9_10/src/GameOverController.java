ad2984411609d7aa487efaded3113668be198e6e
kobocawaee <112955078+kobocawaee@users.noreply.github.com>
2024-12-18 15:16:43 +0800

    加入背景音效，新增playagain判斷是三排還是四排的功能

--- G9_10/src/GameOverController.java
+++ G9_10/src/GameOverController.java
@@ -7,16 +7,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class GameOverController {
-    @FXML
-    private Button button_play_again,button_title;
-    @FXML
-    private Label label_final_score,label_combo;
+    @FXML private Button button_play_again,button_title;
+    @FXML private Label label_final_score,label_combo;
+    @FXML AnchorPane gameover;
+    
+    int play;
+    Parent root;
+    private MediaPlayer backgroundMusicPlayer;
+    
     @FXML
     public void Click_title(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuController.fxml"));
+        backgroundMusicPlayer.stop();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Menu.fxml"));
         Parent root = loader.load();
         
         // 取得目前舞台
@@ -30,8 +44,15 @@ public class GameOverController {
 
     @FXML
     public void Click_Play_Again(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("PlayController.fxml"));
-        Parent root = loader.load();
+        backgroundMusicPlayer.stop();
+        if(play==1){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Play1.fxml"));
+            root = loader.load();
+        }
+        else{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Play2.fxml"));
+            root = loader.load();
+        }
         
         // 取得目前舞台
         Stage stage = (Stage) button_play_again.getScene().getWindow();
@@ -42,8 +63,20 @@ public class GameOverController {
         stage.show();
     }
     @FXML
-    public void initialize(int score, int combo){
+    public void initialize(int score, int combo, int num){
+        Image img = new Image(getClass().getResource("gameover_image.png").toExternalForm()); //背景圖片
+        BackgroundImage bImg = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT,
+        BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        Background bGround = new Background(bImg);
+        gameover.setBackground(bGround);
+        
         label_final_score.setText(String.valueOf(score));
         label_combo.setText(String.valueOf(combo));
+        
+        play = num;
+        Media backgroundMusic = new Media(getClass().getResource("gameover_music.wav").toExternalForm());
+        backgroundMusicPlayer = new MediaPlayer(backgroundMusic);
+        backgroundMusicPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        backgroundMusicPlayer.play();
     }
 }
